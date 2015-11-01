f765576f0ce524d286be7276596ea0faa64cb9aa
Sergey <sramator@gmail.com>
2015-11-01 20:56:56 +0200

    Add public options

--- dev/js/app.js
+++ dev/js/app.js
@@ -7,29 +7,75 @@ console.log('hi');
 ;
 (function ($) {
 
-    $(window).load(function () {
+    var slider = [],
+        sliderWrap = [],
+        sliderList = [],
+        sliderItem = [],
+        sliderImg = [],
+        sliderWrapWidth = 0,
 
-        var slider = $('.slider'),
-            sliderWrap = [],
-            sliderList = [],
-            sliderItem = [],
-            sliderImg = [],
-            sliderWrapWidth = '',
-
-            defaults = {
-                slideCount: 4,
-                controls: true,
-                prevText: 'prev',
-                nextText: 'next'
-            },
-            opt = {};
-
-        var config = $.extend({}, defaults, opt);
-
-        init();
+        defaults = {
+            slideCount: 4,
+            controls: true,
+            prevText: 'prev',
+            nextText: 'next'
+        },
+        options = {};
 
 
-        function init() {
+    function setUpListener() {
+        $('.button').on('click', slide);
+    }
+
+    function setSliderWidth() {
+        sliderWrapWidth = sliderItem.outerWidth(true) * options.slideCount;
+        sliderWrap.width(sliderWrapWidth);
+    }
+
+    function addControlsButton(controls) {
+        if (!controls) return;
+
+        var nextButton = $('<a>', {
+                class: 'button button-next',
+                text: defaults.nextText
+            }),
+            prevButton = $('<a>', {
+                class: 'button button-prev',
+                text: defaults.prevText
+            });
+        nextButton.add(prevButton).appendTo(slider);
+    }
+
+    function slide() {
+        if ($(this).hasClass('button-prev')) {
+            prevSlide();
+        } else {
+            nextSlide();
+        }
+    }
+
+    function nextSlide() {
+        var posLeft = sliderList.css('left');
+
+        if (Math.abs(parseInt(posLeft)) >= parseInt(sliderWrapWidth)) return;
+        sliderList.css({
+            left: parseInt(posLeft) - sliderItem.outerWidth(true)
+        });
+    }
+
+    function prevSlide() {
+        var posLeft = sliderList.css('left');
+
+        if (parseInt(posLeft) >= 0) return;
+        sliderList.css({
+            left: parseInt(posLeft) + sliderItem.outerWidth(true)
+        });
+    }
+
+
+    window.slider = {
+        init: function (opt) {
+            options = $.extend({}, defaults, opt);
             slider = $('.slider');
             sliderWrap = slider.find('.slider__wrapper');
             sliderList = slider.find('.slider__list');
@@ -42,56 +88,10 @@ console.log('hi');
             setUpListener();
 
         }
-
-        function setUpListener() {
-            $('.button').on('click', slide);
-        }
-
-        function setSliderWidth() {
-            sliderWrapWidth = sliderItem.outerWidth(true) * config.slideCount;
-            sliderWrap.width(sliderWrapWidth);
-        }
-
-        function addControlsButton(controls) {
-            if (!controls) return;
-
-            var nextButton = $('<a>', {
-                    class: 'button button-next',
-                    text: defaults.nextText
-                }),
-                prevButton = $('<a>', {
-                    class: 'button button-prev',
-                    text: defaults.prevText
-                });
-            nextButton.add(prevButton).appendTo(slider);
-        }
-
-        function slide() {
-            if ($(this).hasClass('button-prev')) {
-                prevSlide();
-            } else {
-                nextSlide();
-            }
-        }
-
-        function nextSlide() {
-            var posLeft = sliderList.css('left');
-            if (Math.abs(parseInt(posLeft)) >= parseInt(sliderWrapWidth)) return;
-            sliderList.css({
-                left: parseInt(posLeft) - sliderItem.outerWidth(true)
-            });
-
-        }
-
-        function prevSlide() {
-            var posLeft = sliderList.css('left');
-            if (parseInt(posLeft) >= 0) return;
-            sliderList.css({
-                left: parseInt(posLeft) + sliderItem.outerWidth(true)
-            });
-
-        }
-
-    })
+    }
 
 })(jQuery);
+
+slider.init({
+    slideCount: 2
+});

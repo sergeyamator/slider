628db1e417527ad303edf5f7b40c46c0564c893c
Sergey Rubets <srubets@speroteck.com>
2015-11-27 11:33:06 +0200

    Refactoring.

--- dev/js/app.js
+++ dev/js/app.js
@@ -7,125 +7,135 @@ console.log('hi');
 ;
 (function ($) {
 
-    var slider = [],
-        sliderWrap = [],
-        sliderList = [],
-        sliderItem = [],
-        sliderImg = [],
-        sliderWrapWidth = 0,
-        stopNextValue = 0,
-        canSlide = true,
+  var slider = [],
+    sliderWrap = null,
+    sliderList = null,
+    sliderItem = null,
+    sliderImg = null,
+    sliderWrapWidth = 0,
+    stopNextValue = 0,
+    canSlide = true,
+    autoSlideMoveId = 0,
 
-        defaults = {
-            slideCount: 4,
-            controls: true,
-            prevText: 'prev',
-            nextText: 'next',
-            auto: false,
-            autoTime: 2000
-        },
+    defaults = {
+      slideCount: 4,
+      controls: true,
+      prevText: 'prev',
+      nextText: 'next',
+      autoSlideMove: false,
+      autoTime: 2000
+    },
 
-        options = {};
+    options = {};
 
-    function setUpListener() {
-        $('.button').on('click', slide);
+  function setUpListener() {
+    $('.button').on('click', slide);
+    sliderList.on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function () {
+      canSlide = true;
+    })
+
+  }
+
+  function setSliderWidth() {
+    sliderWrapWidth = sliderItem.outerWidth(true) * options.slideCount;
+    sliderWrap.width(sliderWrapWidth);
+  }
+
+  function addControlsButton(controls) {
+    if (!controls) return;
+
+    var nextButton = $('<a>', {
+        class: 'button button-next',
+        'data-button': 'next',
+        text: defaults.nextText
+      }),
+
+      prevButton = $('<a>', {
+        class: 'button button-prev',
+        'data-button': 'prev',
+        text: defaults.prevText
+      });
+
+    nextButton.add(prevButton).appendTo(slider);
+  }
+
+  function slide() {
+    stopNextValue = sliderItem.outerWidth(true) * (sliderItem.length - options.slideCount);
+
+    if (!canSlide) return;
+    canSlide = false;
+
+    if ($(this).attr('data-button') === 'prev') {
+      prevSlide();
+    } else {
+      nextSlide();
     }
 
-    function setSliderWidth() {
-        sliderWrapWidth = sliderItem.outerWidth(true) * options.slideCount;
-        sliderWrap.width(sliderWrapWidth);
+  }
+
+  function autoSlideMove() {
+    if (options.autoSlideMove) {
+      var next = $('.button-next');
+      autoSlideMoveId = setInterval(function () {
+        next.click()
+      }, options.autoTime)
     }
+  }
 
-    function addControlsButton(controls) {
-        if (!controls) return;
+  function autoSlideStop() {
+    clearInterval(autoSlideMoveId);
+  }
 
-        var nextButton = $('<a>', {
-                class: 'button button-next',
-                text: defaults.nextText
-            }),
+  function nextSlide() {
+    var posLeft = sliderList.css('left');
 
-            prevButton = $('<a>', {
-                class: 'button button-prev',
-                text: defaults.prevText
-            });
-
-        nextButton.add(prevButton).appendTo(slider);
+    if (Math.abs(parseInt(posLeft)) >= stopNextValue) {
+      sliderList.css({
+        left: 0
+      })
+    } else {
+      sliderList.css({
+        left: parseInt(posLeft) - sliderItem.outerWidth(true)
+      });
     }
+  }
 
-    function slide() {
-        stopNextValue = sliderItem.outerWidth(true) * (sliderItem.length - options.slideCount);
+  function prevSlide() {
+    var posLeft = sliderList.css('left');
 
-        if (!canSlide) return;
-        canSlide = false;
-
-        if ($(this).hasClass('button-prev')) {
-            prevSlide();
-        } else {
-            nextSlide();
-        }
-
-        sliderList.on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function () {
-            canSlide = true;
-        })
+    if (parseInt(posLeft) >= 0) {
+      sliderList.css({
+        left: -stopNextValue + 'px'
+      })
+    } else {
+      sliderList.css({
+        left: parseInt(posLeft) + sliderItem.outerWidth(true)
+      });
     }
+  }
 
-    function autoSlide() {
-        if (options.auto) {
-            var next = $('.button-next');
-            setInterval(function () {
-                next.click()
-            }, options.autoTime)
-        }
-    }
+  window.slider = {
+    init: function (_options) {
+      options = $.extend({}, defaults, _options);
+      slider = $('.slider');
+      sliderWrap = slider.find('.slider__wrapper');
+      sliderList = slider.find('.slider__list');
+      sliderItem = slider.find('.slider__item');
+      sliderImg = slider.find('.slider__img');
 
-    function nextSlide() {
-        var posLeft = sliderList.css('left');
+      setSliderWidth();
+      addControlsButton(defaults.controls);
+      autoSlideMove();
+      setUpListener();
+    },
 
-        if (Math.abs(parseInt(posLeft)) >= stopNextValue) {
-            sliderList.css({
-                left: 0
-            })
-        } else {
-            sliderList.css({
-                left: parseInt(posLeft) - sliderItem.outerWidth(true)
-            });
-        }
-    }
-
-    function prevSlide() {
-        var posLeft = sliderList.css('left');
-
-        if (parseInt(posLeft) >= 0) {
-            sliderList.css({
-                left: -stopNextValue + 'px'
-            })
-        } else {
-            sliderList.css({
-                left: parseInt(posLeft) + sliderItem.outerWidth(true)
-            });
-        }
-    }
-
-    window.slider = {
-        init: function (opt) {
-            options = $.extend({}, defaults, opt);
-            slider = $('.slider');
-            sliderWrap = slider.find('.slider__wrapper');
-            sliderList = slider.find('.slider__list');
-            sliderItem = slider.find('.slider__item');
-            sliderImg = slider.find('.slider__img');
-
-            setSliderWidth();
-            addControlsButton(defaults.controls);
-            autoSlide();
-            setUpListener();
-        }
-    }
+    autoSlideStop: autoSlideStop
+  }
 
 })(jQuery);
 
 slider.init({
-    slideCount: 3,
-    auto: true,
-    autoTime: 5000
+  slideCount: 3,
+  autoSlideMove: true,
+  autoTime: 5000
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,206 +1,102 @@
 'use strict';
 
 var gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    browserSync = require('browser-sync'),
-    uglify = require('gulp-uglify'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    autoprefixer = require('gulp-autoprefixer'),
-    jade = require('gulp-jade'),
-    minifyCss = require('gulp-minify-css'),
-    rename = require("gulp-rename"),
-    imagemin = require('gulp-imagemin'),
-    pngquant = require('imagemin-pngquant'),
-    spritesmith = require('gulp.spritesmith'),
-    plumber = require('gulp-plumber');
+  concat = require('gulp-concat'),
+  browserSync = require('browser-sync'),
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  jade = require('gulp-jade'),
+  plumber = require('gulp-plumber');
 
 
 /* --------- paths --------- */
 var paths = {
-    scss: {
-        location: ['dev/scss/*.scss'],
-        destination: 'dev/css'
-    },
+  scss: {
+    location: ['dev/scss/*.scss'],
+    destination: 'dev/css'
+  },
 
-    js: {
-        location: ['dev/js/modules/*.js'],
-        destination: 'dev/js'
-    },
+  js: {
+    location: ['dev/js/modules/*.js'],
+    destination: 'dev/js'
+  },
 
-    bowerCss: {
-        location: [
-            'bower_components/normalize-css/normalize.css'
-        ],
-        destination: 'dev/css'
-    },
+  bowerCss: {
+    location: [
+      'bower_components/normalize-css/normalize.css'
+    ],
+    destination: 'dev/css'
+  },
 
-    bowerJS: {
-        location: [
-            'bower_components/jquery/dist/jquery.js'
-        ],
-        destination: 'dev/js'
-    }
+  bowerJS: {
+    location: [
+      'bower_components/jquery/dist/jquery.js'
+    ],
+    destination: 'dev/js'
+  }
 };
 
 
 /* ----- jade ----- */
 gulp.task('jade', function () {
-    gulp.src(['dev/jade/[^_]*.jade'])
-        .pipe(plumber())
-        .pipe(jade({
-            pretty: true
-        }))
-        .pipe(gulp.dest('dev/'))
+  gulp.src(['dev/jade/[^_]*.jade'])
+    .pipe(plumber())
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('dev/'))
 });
 
 
 /* ------ sass ------ */
 gulp.task('sass', function () {
-    gulp.src(paths.scss.location)
-        .pipe(plumber())
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(paths.scss.destination));
-});
-
-
-/* -------- minification CSS -------- */
-gulp.task('minify-css', function () {
-    return gulp.src('dev/css/main.css')
-        .pipe(minifyCss({compatibility: 'ie8'}))
-        .pipe(rename("main.min.css"))
-        .pipe(gulp.dest('prod/css'));
-});
-
-
-/* -------- autoprefixer -------- */
-gulp.task('autpr', function () {
-    return gulp.src('prod/css/main.min.css')
-        .pipe(autoprefixer(['> 5%', 'last 5 versions', 'IE 9']))
-        .pipe(gulp.dest('prod/css'));
+  gulp.src(paths.scss.location)
+    .pipe(plumber())
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.scss.destination));
 });
 
 
 
-/* -------- concat CSS -------- */
-gulp.task('concat-bower-css', function () {
-    return gulp.src(paths.bowerCss.location)
-        .pipe(concat('vendor.css'))
-        .pipe(gulp.dest(paths.bowerCss.destination));
-});
-
-
-/* -------- minification Vendor CSS -------- */
-gulp.task('minify-vendor', function () {
-    return gulp.src('dev/css/vendor.css')
-        .pipe(minifyCss({compatibility: 'ie8'}))
-        .pipe(rename("vendor.min.css"))
-        .pipe(gulp.dest('prod/css'));
-});
-
-
-
-/* -------- concat JS -------- */
-gulp.task('concat-JS', function () {
-    return gulp.src(paths.js.location)
-        .pipe(concat('app.js'))
-        .pipe(gulp.dest(paths.js.destination));
-});
-
-/* -------- concat Vendor JS -------- */
-gulp.task('concat-bower-js', function () {
-    return gulp.src(paths.bowerJS.location)
-        .pipe(concat('vendor.js'))
-        .pipe(gulp.dest(paths.bowerJS.destination));
-});
-
-
-/* -------- minification JS -------- */
-gulp.task('compress', function () {
-    return gulp.src('dev/js/app.js')
-        .pipe(uglify())
-        .pipe(rename("app.min.js"))
-        .pipe(gulp.dest('prod/js'));
-});
-
-
-/* -------- minification Bower JS -------- */
-gulp.task('compress-vendor-js', function () {
-    return gulp.src('dev/js/vendor.js')
-        .pipe(uglify())
-        .pipe(rename("vendor.min.js"))
-        .pipe(gulp.dest('prod/js'));
-});
-
-
-/* -------- auto sprites  -------- */
-gulp.task('sprite', function () {
-    var spriteData = gulp.src('dev/img/icons/*.png').pipe(spritesmith({
-        imgName: 'sprite.png',
-        imgPath: '../img/sprite.png',
-        cssName: 'sprite.scss'
-    }));
-    spriteData.img.pipe(gulp.dest('dev/img/'));
-    spriteData.css.pipe(gulp.dest('dev/scss/'));
-});
-
-
-/* -------- images minification  -------- */
-gulp.task('imagemin', function () {
-    return gulp.src('dev/img/**/*')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest('prod/img'));
+/* -------- concat js -------- */
+gulp.task('concat-js', function () {
+  return gulp.src(paths.js.location)
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest(paths.js.destination));
 });
 
 
 /* -------- gulp server  -------- */
 gulp.task('server', function () {
-    browserSync({
-        port: 9000,
-        server: {
-            baseDir: 'dev'
-        }
-    });
+  browserSync({
+    port: 9000,
+    server: {
+      baseDir: 'dev'
+    }
+  });
 });
 
 
 /* -------- gulp watching  -------- */
 gulp.task('watch', function () {
-    gulp.watch('dev/jade/*.jade', ['jade']);
-    gulp.watch('dev/scss/*.scss', ['sass']);
-    gulp.watch('dev/js/modules/*.js', ['concat-JS']);
-    gulp.watch([
-        'dev/index.html',
-        'dev/js/**/*.js',
-        'dev/css/**/*.css'
-    ]).on('change', browserSync.reload);
+  gulp.watch('dev/jade/*.jade', ['jade']);
+  gulp.watch('dev/scss/*.scss', ['sass']);
+  gulp.watch('dev/js/modules/*.js', ['concat-js']);
+  gulp.watch([
+    'dev/index.html',
+    'dev/js/**/*.js',
+    'dev/css/**/*.css'
+  ]).on('change', browserSync.reload);
 });
 
 
 gulp.task('default', [
-    'jade',
-    'sass',
-    'concat-JS',
-    'concat-bower-css',
-    'concat-bower-js',
-    'compress',
-    'watch',
-    'server'
-]);
-
-
-gulp.task('prod', [
-    'minify-css',
-    'autpr',
-    'minify-vendor',
-    'compress',
-    'compress-vendor-js',
-    'imagemin'
+  'jade',
+  'sass',
+  'concat-js',
+  'watch',
+  'server'
 ]);
 
 
